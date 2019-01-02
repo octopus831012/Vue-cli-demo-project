e1d9c618bb3aecfb5e61efe7b5b6b2951081d22b
Yvonne <yvonne@gmail.com>
2019-01-02 16:08:12 +0800

    creating color data

---
+++ src/components/ScssColorCardTool/main.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'ScssColorCardTool',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      colorSetting: 'color_pink: #ffd9ec\ncolor_purple: #dcb5ff\ncolor_red: #ffb5b5\ncolor_blue: #aaaaff\ncolor_green: #93ff93',
+    };
+  },
+};
