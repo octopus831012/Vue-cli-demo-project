de060569fac5b4464622a6eb769e67cf7f840d07
Yvonne <yvonne@gmail.com>
2019-01-02 17:18:42 +0800

    analyzing data and showing the color

--- src/components/ScssColorCardTool/main.js
+++ src/components/ScssColorCardTool/main.js
@@ -8,4 +8,23 @@ export default {
       colorSetting: 'color_pink: #ffd9ec\ncolor_purple: #dcb5ff\ncolor_red: #ffb5b5\ncolor_blue: #aaaaff\ncolor_green: #93ff93',
     };
   },
+  computed: {
+    colorCards() {
+      const result = [];
+      const cutString = this.colorSetting.split('\n');
+      for (let i = 0; i < cutString.length; i += 1) {
+        const name = cutString[i].split(':')[0];
+        const color = cutString[i].split(' ')[1];
+
+        result.push({
+          name,
+          color,
+          colorCSS: {
+            'background-color': color,
+          },
+        });
+      }
+      return result;
+    },
+  },
 };
